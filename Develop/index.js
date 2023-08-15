ca0b4587c2786fa5aa6a59779eedc849f16d1350
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-08-15 16:02:59 -0700

    added several questons to readme

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,53 @@ const inquirer = require('inquirer');
 const path = require('path')
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Please name your Project',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Please describe the purpose and functionality of this project.',
+        name: 'description',
+      },
+      {
+        type: 'checkbox',
+        message: 'Please select a license applicable to this project.',
+        name: 'License',
+        chioces: ['MIT','APACHE2.0','Boost1.0','MPL2.0','BSD2','BSD3','none'],
+      },
+      {
+        type: 'input',
+        message: 'List any project dependencies here.',
+        name: 'require',
+      },
+      {
+        type: 'input',
+        message: 'State the languages or technologies associated with this project.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'creator',
+      },
+      {
+        type: 'input',
+        message: 'State your full name.',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'Provide a valid email address',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'Provide walkthrough of required test if applicable.',
+        name: 'test',
+      },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
