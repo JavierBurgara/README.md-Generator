23579a92033941773610147fb22fde884df18cd4
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-08-17 15:49:02 -0700

    added different path ways to my folders and added license bar which i didnt have last time

--- README.md
+++ README.md
@@ -1,32 +1,33 @@
-# README-Generator
-  by Javier Burgara
-  ![Github license](https://img.sheilds.io/badge/license-undefined-yellowgreen.svg)
-  ## Table of Contents
-  * [Description](#description)
-  *[Requirements](#requirements)
-  * [Usage](#usage)
-  * [Contact-Me](#contact-me)
-  * [Contributors](#contributors)
-  * [Testing](#testing)
-  
-* [license](#license)
-
-  ## Description
-  using node and understanding how you call back data and use it
-  ## Requirement
-  Inquirer 8.2.4
-  ## Usage
-  JavaScript,Node,JSON
-  ## Contact-Me
-  * Name - Javier Burgara
+# Readme-Generator 
+![Github license](https://img.shields.io/badge/license--blue.svg)
+## Description
+Using node and javascript to create a professional readme
+## Deployed Application URL
+https://github.com/JavierBurgara/README.md-Generator
+## Screenshot
+![alt-text](N/A)
+## Table of Contents
+* [Description](#description)
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+*[Contact-Me](#contact-me)
+## Features
+Helps people use my readme-generator to create professional readme for any project you have.
+## Languages & Dependencies
+N/A
+## How to Use This Application:
+Node and Javascript
+## Contributors
+N/A
+## Testing
+N/A
+## Contact-Me
+  * Name - JavierBurgara
   * Email - Javierburgara150@yahoo.com
   * Github - [JavierBurgara](https://github.com/JavierBurgara/)
-  ## Contributors
-  undefined
-  ## Testing
-  ```
-  N/A
-  ```
-  ## License
-      Licensed under the undefined license.
-  
+## Questions
+Please send your questions [here](mailto:Javierburgara150@yahoo.com?subject=[GitHub]%20Dev%20Connect) or visit [github/JavierBurgara](https://github.com/JavierBurgara).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,62 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Creating a function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
-  if(license !== "none"){
-    return `![Github license](https://img.sheilds.io/badge/license-${license}-yellowgreen.svg)`;
-  }
-  return "";
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
   if (license !== "none") {
-    return `\n* [license](#license)\n`;
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
   return "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== "none") {
-    return `## License
-      Licensed under the ${license} license.`;
-  }
-  return "";
-}
-
-// TODO: Create a function to generate markdown for README
+//Creating a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  by ${data.name}
-  ${renderLicenseBadge(data.license)}
-  ## Table of Contents
-  * [Description](#description)
-  *[Requirements](#requirements)
-  * [Usage](#usage)
-  * [Contact-Me](#contact-me)
-  * [Contributors](#contributors)
-  * [Testing](#testing)
-  ${renderLicenseLink(data.license)}
-  ## Description
-  ${data.description}
-  ## Requirement
-  ${data.require}
-  ## Usage
-  ${data.usage}
-  ## Contact-Me
+  return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Table of Contents
+* [Description](#description)
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+*[Contact-Me](#contact-me)
+## Features
+${data.features}
+## Languages & Dependencies
+${data.require}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contributors}
+## Testing
+${data.test}
+## Contact-Me
   * Name - ${data.name}
   * Email - ${data.email}
   * Github - [${data.creator}](https://github.com/${data.creator}/)
-  ## Contributors
-  ${data.contributors}
-  ## Testing
-  \`\`\`
-  ${data.test}
-  \`\`\`
-  ${renderLicenseSection(data.license)}
-  `;
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+`;
 }
 
 module.exports = generateMarkdown;
